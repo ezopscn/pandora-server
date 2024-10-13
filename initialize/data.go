e3e1e46b95585e2ab0836408884b55fa9d42a79b
DK <ezopscn@gmail.com>
2024-10-13 23:05:42 +0800

    完善用户模型

--- initialize/data.go
+++ initialize/data.go
@@ -2,10 +2,12 @@ package initialize
 
 import (
 	"fmt"
+	"pandora-server/global"
+	"time"
 )
 
 // 系统基础数据初始化
 func MigrateData() {
-	fmt.Println("开始进行系统基础数据初始化")
-	fmt.Println("系统基础数据初始化完成")
+	fmt.Println("开始进行系统基础数据初始化：" + time.Now().Format(global.MillisecondTimeFormat))
+	fmt.Println("系统基础数据初始化完成：" + time.Now().Format(global.MillisecondTimeFormat))
 }
--- initialize/table.go
+++ initialize/table.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"pandora-server/global"
 	"pandora-server/model"
+	"time"
 )
 
 // 数据结构同步
 func MigrateTable() {
-	fmt.Println("开始进行数据结构（数据表）同步")
+	fmt.Println("开始进行数据结构（数据表）同步：" + time.Now().Format(global.MillisecondTimeFormat))
 	_ = global.MySQLDB.AutoMigrate(new(model.SystemUser)) // 用户
-	fmt.Println("数据结构（数据表）同步完成")
+	fmt.Println("数据结构（数据表）同步完成：" + time.Now().Format(global.MillisecondTimeFormat))
 }
